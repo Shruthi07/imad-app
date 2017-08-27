0652a4889a6c41be3c0c0a0708979ae306746df1
Shruthi07 <30904790+Shruthi07@users.noreply.github.com>
2017-08-27 18:03:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send("Article 1 requested and sent");
 });
 
 app.get('/ui/style.css', function (req, res) {
